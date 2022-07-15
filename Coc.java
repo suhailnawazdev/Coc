4d6b991e517f77a0799ba3b15efc0eb0a1819e3e
suhailnawazdev <109364334+suhailnawazdev@users.noreply.github.com>
2022-07-15 21:08:05 +0530

    Add files via upload

---
+++ Coc.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class Coc
+
+{
+	public static void main(String arg[])
+	
+	
+	{
+		Double r,coc;
+		
+		System.out.println("Enter the Circumference");
+		
+		Scanner s = new Scanner (System.in);
+		
+		r=s.nextDouble();
+		
+		coc=2*3.14*r;
+		
+		System.out.println("Circumference is" + coc);
+		
+		System.out.println("coc is" + coc);
+		
+		
+	}
+	
+	
+}
+
